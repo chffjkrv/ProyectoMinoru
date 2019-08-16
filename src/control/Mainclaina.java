ecb8f9409f17bf2a1b985841b62015d4498317b5
chffjkrv <daniel.arroyave.22@gmail.com>
2019-08-16 15:26:14 +0200

    Mejorando hashmap y limpiando main

--- src/control/Mainclaina.java
+++ src/control/Mainclaina.java
@@ -6,9 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
-
-
 import utilidades.Searcher;
 import utilidades.Hashmap;
 import utilidades.HerramientasTexto;
@@ -17,46 +16,28 @@ import utilidades.HerramientasTexto;
 public class Mainclaina {
 
 	public static void main(String[] args) throws IOException {
-    		
-		/** PRUEBA DE METODO SEARCHER
-		
-		Searcher searchEngine = new Searcher();
-        String[] matches;
-        matches = searchEngine.search(
-                "Perro Verde",
-               
-                "El Perro Verde nace en el año 2005 en colaboración "+
-                "de Ángel Lorenzo Cachazo (Martivilli) junto Verde con su"+
-                " hijo Javier, con Perro Verde Perro Vila Viniteca. Es uno de los proyectos ");
-        searchEngine.printMatches(matches);
-	}
-	*/
-	
-		ArrayList<String> arrTxt = HerramientasTexto.fileToArray("SunenCríticasLiterarias.txt");
+
+
+		ArrayList<String> arrTxt = HerramientasTexto.fileToArray("BIBLIACOMPLETA.txt");
 		ArrayList<String> arrTxt2 = new ArrayList<String>();
 
-		/**for (int x =0; x<arrTxt.size(); x++){
-    	    System.out.println(arrTxt.get(x));
-    	} */
 		arrTxt2 = HerramientasTexto.quitarRepeticiones(arrTxt);
-		
+
 		for (int x =0; x<arrTxt2.size(); x++){
     	    System.out.println(arrTxt2.get(x));
-    	} 
-		System.out.println("Primer array: "+arrTxt.size()+"\n");
-		System.out.println("Segudno array sin repes: "+arrTxt2.size());
-		
+		}
 		 HashMap<Integer,String> listaPalabras;
-		
+
 		Hashmap Listpal = new Hashmap();
-		
+
 		listaPalabras = Listpal.creaHashmap();
-		for (int x =0; x<arrTxt2.size(); x++){
-			Listpal.guardarPalabra(x, arrTxt2.get(x), listaPalabras);
-    	} 
+		Listpal.llenarHashMap(listaPalabras, arrTxt2);
+ 		Listpal.mostrarPalabras2(listaPalabras);
+    	
+		System.out.println("Primer array: "+arrTxt.size()+"\n");
+		System.out.println("Segundo array sin repes: "+arrTxt2.size());
 		
-		Listpal.mostrarPalabras(listaPalabras);
-		
+
 		
 	}
 }
--- src/utilidades/Hashmap.java
+++ src/utilidades/Hashmap.java
@@ -15,7 +15,10 @@ public class Hashmap {
    public Hashmap() {
 
    }
-
+/**
+ * 
+ * @return retorna un HashMap clave Integer, valor String.
+ */
    public  HashMap<Integer,String> creaHashmap() {
 	   HashMap<Integer,String> listaPalabras = new HashMap<Integer,String>();
 	   return listaPalabras;
@@ -23,11 +26,19 @@ public class Hashmap {
 
    public void guardarPalabra(Integer codigo, String palabra, HashMap <Integer,String> listaPalabras){
 	    if (listaPalabras.containsKey(codigo)) {
-	        System.out.println("No se puede introducir el Palabra. El c�digo esta repetido.");
+	        System.out.println("No se puede introducir el Palabra. El codigo esta repetido.");
 	    } else {
 	        listaPalabras.put(codigo, palabra);
 	    }
 	}
+   
+   public void llenarHashMap(HashMap <Integer,String> lista, ArrayList<String> arrtxt2 ) {
+	 
+	   Hashmap Listpal = new Hashmap();
+		for (int x =0; x<arrtxt2.size(); x++){
+			Listpal.guardarPalabra(x, arrtxt2.get(x), lista);
+		}   
+   }
 
    public  void modificaPalabra(Integer codigo, HashMap<Integer,String> listaPalabras){
 	    Scanner sc = new Scanner(System.in);
@@ -35,7 +46,7 @@ public class Hashmap {
 	        System.out.println("Introduce la palabra que quieras que tenga este codigo:");
 	        listaPalabras.put(codigo, sc.next());
 	    } else {
-	        System.out.println("No hay ninguna Palabra con ese c�digo.");
+	        System.out.println("No hay ninguna Palabra con ese codigo.");
 	    }
 	}
 
@@ -58,19 +69,33 @@ public class Hashmap {
 	        System.out.println(Palabra.getKey() + " - " + Palabra.getValue());
 	    }
 	}
+   public Integer damelId(HashMap<Integer, String> listaPalabras, String pal) {
+	    
+	   Iterator iterador = listaPalabras.entrySet().iterator();
+	   Integer id;
+	   
+	    Map.Entry Palabra;
+	    while (iterador.hasNext()) {
+	        Palabra = (Map.Entry) iterador.next();
+	        id = Palabra.getKey();
+	        String palInterna = (String)Palabra.getValue());
+	    }
+	    return id;
+	}
 
    public  void eliminaPalabra(Integer codigo, HashMap<Integer,String> listaPalabras) {
 	    if (listaPalabras.containsKey(codigo)) {
 	        listaPalabras.remove(codigo);
 	    } else {
-	        System.out.println("No hay ningun Palabra con ese c�digo.");
+	        System.out.println("No hay ninguna palabra con ese codigo.");
 	    }
 	}
 
    public Integer[] arrTxtTOidArrTxt(ArrayList<String> arrTxt) {
 	   Integer[] intArrtxt = new Integer[arrTxt.size()];
+	   
 	   for(int i=0;i<arrTxt.size();i++) {
-
+		   
 	   }
 
 	   return intArrtxt;
